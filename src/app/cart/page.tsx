dafb28df3af6320bfa4f898d9a2ad9ab0d9707ec
karengomezs <karengomezsoto1@gmail.com>
2023-06-20 13:25:49 -0500

    minor

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -8,10 +8,13 @@ export default function Cart() {
 
   const dataArr = new Set(cartState?.items);
   let products = [...dataArr].sort((a, b) => {
+    //este sort es para ordenarlos en la página donde se renderiza por los id de cada elemento
     return a.id.localeCompare(b.id);
   });
 
   const cartProducts = products.map((product) => {
+    //comparo el id de cada item del arreglo que está en el context, con los id del nuevo array sin
+    //objetos duplicados, y solo me devuelve solo los elementos que coinciden por id comparando los dos arr
     const idRep = cartState.items.filter((p) => p.id === product.id);
 
     return (
@@ -31,10 +34,18 @@ export default function Cart() {
           <p className="font-bold text-rose-600">$ {product.price}</p>
           <div className="flex gap-4  rounded-md [&>*]:bg-slate-300 [&>*]:px-4 [&>*]:rounded-md">
             <button>-</button>
+            {/* por cada iteración, saco el largo del array que me da las coincidencias entre ids */}
             <p>{idRep.length}</p>
             <button
               onClick={() => {
-                cartState.setItems([product, ...cartState.items]);
+                //cada vez que clickeo el botón de "+" vuelvo y agrego el elemento al context
+                // cartState.setItems([product, ...cartState.items]);
+                //ese parámetro es el estado actual, current items es el último estado que haya quedado
+                //en el estado, es lo mismo que la línea de arriba pero esta es la forma recomendada
+                cartState.setItems((currentItems) => [
+                  product,
+                  ...currentItems,
+                ]);
               }}
             >
               +
