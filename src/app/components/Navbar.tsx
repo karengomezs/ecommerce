16b959ac16265a7fa38541bfa007847a4c8115f7
karengomezs <karengomezsoto1@gmail.com>
2023-06-22 18:55:55 -0500

    cart icon component

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { UserButton } from "@clerk/nextjs";
+import CartIcon from "./CartIcon";
 
 export default function Navbar() {
   return (
@@ -9,27 +10,11 @@ export default function Navbar() {
           <p className=" px-3 py-1 border rounded-full">K</p>
           <div className="ms-4 gap-3 ">
             <p>SHOP</p>
-            {/* <p className="">MAKING YOU HAPPY</p> */}
           </div>
         </Link>
         <ul className="flex justify-between items-center w-40 ">
           <li>
-            <Link href="/cart">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-8 h-8 "
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
-                />
-              </svg>
-            </Link>
+            <CartIcon />
           </li>
           <li>
             <Link href="/create-product">
