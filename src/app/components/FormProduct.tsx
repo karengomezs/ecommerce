52a1c01e3a32f2a188b486bbc164701824f74db4
karengomezs <karengomezsoto1@gmail.com>
2023-06-24 13:00:25 -0500

    inputs working

--- src/app/components/FormProduct.tsx
+++ src/app/components/FormProduct.tsx
@@ -1,10 +1,71 @@
 "use client";
-import { saveProduct, uploadImage } from "@/api/products";
-import { useUser } from "@clerk/nextjs";
-import * as Form from "@radix-ui/react-form";
 import { ChangeEvent, useState, useRef } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as Form from "@radix-ui/react-form";
+import { useUser } from "@clerk/nextjs";
+import { saveProduct, uploadImage } from "@/api/products";
+
+const schema = z
+  .object({
+    name: z.string().min(3, { message: "Product name is required!" }),
+    price: z.number(),
+    img: z.object({ file: z.instanceof(File) }),
+  })
+  .required();
+
+type Form = z.infer<typeof schema>;
 
 export default function FormProduct() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Form>({ resolver: zodResolver(schema) });
+
+  console.log(errors);
+
+  const onSubmit: SubmitHandler<Form> = async (data) => {
+    try {
+      if (!user?.id) return;
+
+      console.log(data);
+
+      // setButtonDisable(true);
+
+      // const img = await uploadImage(productImg);
+
+      // let product: Product = {
+      //   id: "",
+      //   name: productName,
+      //   price: productPrice,
+      //   img: img.urlImg,
+      //   userId: user?.id,
+      //   userName: user?.fullName ?? "",
+      //   date: new Date(),
+      // };
+
+      // const doc = await saveProduct(product);
+      // product.id = doc?.id ?? Date.now().toString();
+
+      //     setPostsArray([post, ...postsArray]);
+      // setProductImg(undefined);
+      // setProductName("");
+      // setProductPrice(0);
+      //this function clear all form elements
+      // ref.current?.reset();
+
+      // setProductPosted(true);
+      // setButtonDisable(false);
+    } catch (error) {
+      // console.error(error);
+      // setButtonDisable(false);
+    }
+  };
+
+  // -----------------------------------
+
   const ref = useRef<HTMLFormElement>(null);
   const { user } = useUser();
   const [productName, setProductName] = useState<string>("");
@@ -19,57 +80,59 @@ export default function FormProduct() {
       <Form.Root
         ref={ref}
         className="flex flex-col gap-4"
-        onSubmit={async (e) => {
-          e.preventDefault();
+        onSubmit={handleSubmit(onSubmit)}
+        // onSubmit={async (e) => {
+        //   e.preventDefault();
 
-          try {
-            if (!productImg || !user?.id) return;
+        //   try {
+        //     if (!productImg || !user?.id) return;
 
-            setButtonDisable(true);
+        //     setButtonDisable(true);
 
-            const img = await uploadImage(productImg);
+        //     const img = await uploadImage(productImg);
 
-            let product: Product = {
-              id: "",
-              name: productName,
-              price: productPrice,
-              img: img.urlImg,
-              userId: user?.id,
-              userName: user?.fullName ?? "",
-              date: new Date(),
-            };
+        //     let product: Product = {
+        //       id: "",
+        //       name: productName,
+        //       price: productPrice,
+        //       img: img.urlImg,
+        //       userId: user?.id,
+        //       userName: user?.fullName ?? "",
+        //       date: new Date(),
+        //     };
 
-            const doc = await saveProduct(product);
-            product.id = doc?.id ?? Date.now().toString();
+        //     const doc = await saveProduct(product);
+        //     product.id = doc?.id ?? Date.now().toString();
 
-            //     setPostsArray([post, ...postsArray]);
-            setProductImg(undefined);
-            setProductName("");
-            setProductPrice(0);
-            //this function clear all form elements
-            ref.current?.reset();
+        //     //     setPostsArray([post, ...postsArray]);
+        //     setProductImg(undefined);
+        //     setProductName("");
+        //     setProductPrice(0);
+        //     //this function clear all form elements
+        //     ref.current?.reset();
 
-            setProductPosted(true);
-            setButtonDisable(false);
-          } catch (error) {
-            console.error(error);
-            setButtonDisable(false);
-          }
-        }}
+        //     setProductPosted(true);
+        //     setButtonDisable(false);
+        //   } catch (error) {
+        //     console.error(error);
+        //     setButtonDisable(false);
+        //   }
+        // }}
       >
         <Form.Field name="name">
           <div className="flex flex-col gap-1">
             <Form.Label className="">Name</Form.Label>
             <Form.Control asChild>
               <input
-                onChange={(e) => {
-                  setProductName(e.target.value);
-                  setProductPosted(false);
-                }}
-                value={productName}
+                {...register("name")}
+                // onChange={(e) => {
+                //   setProductName(e.target.value);
+                //   setProductPosted(false);
+                // }}
+                // value={productName}
                 className="w-full p-2 rounded-md border border-slate-300 bg-slate-100"
-                type="text"
-                required
+                // type="text"
+                // required
               />
             </Form.Control>
           </div>
@@ -80,13 +143,18 @@ export default function FormProduct() {
             <Form.Label className="">Price</Form.Label>
             <Form.Control asChild>
               <input
-                onChange={(e) => {
-                  setProductPrice(parseInt(e.target.value));
-                }}
-                value={productPrice}
+                {...register("price", {
+                  setValueAs: (value) => {
+                    return value === "" ? undefined : parseInt(value, 10);
+                  },
+                })}
+                // onChange={(e) => {
+                //   setProductPrice(parseInt(e.target.value));
+                // }}
+                // value={productPrice}
                 className="w-full p-2 rounded-md border border-slate-300 bg-slate-100"
-                type="text"
-                required
+                type="number"
+                // required
               />
             </Form.Control>
           </div>
@@ -97,12 +165,13 @@ export default function FormProduct() {
             <Form.Label className="">Image</Form.Label>
             <Form.Control asChild>
               <input
-                onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                  setProductImg(e.target.files?.[0]);
-                }}
+                {...register("img")}
+                // onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                //   setProductImg(e.target.files?.[0]);
+                // }}
                 className="w-full p-2 rounded-md border border-slate-300 bg-slate-100"
                 type="file"
-                required
+                // required
               />
             </Form.Control>
           </div>
@@ -110,7 +179,7 @@ export default function FormProduct() {
 
         <Form.Submit asChild>
           <button
-            disabled={buttonDisable}
+            // disabled={buttonDisable}
             type="submit"
             className="w-full mx-auto rounded-md bg-emerald-900 hover:bg-emerald-700 text-white font-bold p-2 my-2"
           >
